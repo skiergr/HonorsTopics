0f6a55f82b2ef0cbeda20a6e272ccad4cb06e79a
Grady <gradyramberg@gmail.com>
2023-08-25 14:24:16 -0700

    Read File

--- fileEditor.java
+++ fileEditor.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -8,6 +10,13 @@ public class fileEditor {
             FileWriter writer = new FileWriter("file.txt");
             writer.write("Hello");
             writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("file.txt"));
+            System.out.println(reader.readLine());
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
